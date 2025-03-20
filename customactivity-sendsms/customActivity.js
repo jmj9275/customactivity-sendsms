9a7052e96a3f4f9c0d983dded20b348408e207d4
Juan Manuel Jimenez <jmi.jimenez.unseld@gmail.com>
2025-03-20 10:31:39 +0100

    update UI for testing

--- customactivity-sendsms/customActivity.js
+++ customactivity-sendsms/customActivity.js
@@ -1,105 +1,111 @@
 define(["postmonger"], function (Postmonger) {
-    "use strict";
-  
-    var connection = new Postmonger.Session();
-    var payload = {};
-  
-    $(window).ready(onRender);
-  
-    connection.on("initActivity", initialize);
-    connection.on("requestedTokens", onGetTokens);
-    connection.on("requestedEndpoints", onGetEndpoints);
-  
-    connection.on("clickedNext", onClickedNext);
-  
-    function onRender() {
-      // JB will respond the first time 'ready' is called with 'initActivity'
-      connection.trigger("ready");
-  
-      connection.trigger("requestTokens");
-      connection.trigger("requestEndpoints");
-  
-      // Disable the next button if a value isn't selected
-      $("#select1").change(function () {
-        var message = getMessage();
-        connection.trigger("updateButton", {
-          button: "done",
-          enabled: Boolean(message),
-        });
-  
-        $("#message").html(message);
+  "use strict";
+
+  var connection = new Postmonger.Session();
+  var payload = {};
+
+  $(window).ready(onRender);
+
+  connection.on("initActivity", initialize);
+  connection.on("requestedTokens", onGetTokens);
+  connection.on("requestedEndpoints", onGetEndpoints);
+
+  connection.on("clickedNext", onClickedNext);
+
+  function onRender() {
+    // JB will respond the first time 'ready' is called with 'initActivity'
+    connection.trigger("ready");
+
+    connection.trigger("requestTokens");
+    connection.trigger("requestEndpoints");
+
+    // Disable the next button if a value isn't selected
+    $("#select1").change(function () {
+      var message = getMessage();
+      connection.trigger("updateButton", {
+        button: "done",
+        enabled: Boolean(message),
       });
+
+      $("#message").html(message);
+    });
+  }
+
+  function initialize(data) {
+    document.getElementById("configuration").value = JSON.stringify(
+      data,
+      null,
+      2
+    );
+
+    if (data) {
+      payload = data;
     }
-  
-    function initialize(data) {
-      if (data) {
-        payload = data;
-      }
-  
-      var message;
-      var hasInArguments = Boolean(
-        payload["arguments"] &&
-          payload["arguments"].execute &&
-          payload["arguments"].execute.inArguments &&
-          payload["arguments"].execute.inArguments.length > 0
-      );
-  
-      var inArguments = hasInArguments
-        ? payload["arguments"].execute.inArguments
-        : {};
-  
-      $.each(inArguments, function (index, inArgument) {
-        $.each(inArgument, function (key, val) {
-          if (key === "message") {
-            message = val;
-          }
-        });
+
+    var message;
+    var hasInArguments = Boolean(
+      payload["arguments"] &&
+        payload["arguments"].execute &&
+        payload["arguments"].execute.inArguments &&
+        payload["arguments"].execute.inArguments.length > 0
+    );
+
+    var inArguments = hasInArguments
+      ? payload["arguments"].execute.inArguments
+      : {};
+
+    $.each(inArguments, function (index, inArgument) {
+      $.each(inArgument, function (key, val) {
+        if (key === "message") {
+          message = val;
+        }
       });
-  
-      // If there is no message selected, disable the next button
-      if (!message) {
-        connection.trigger("updateButton", { button: "done", enabled: false });
-        // If there is a message, skip to the summary step
-      } else {
-        $("#select1")
-          .find("option[value=" + message + "]")
-          .attr("selected", "selected");
-        $("#message").html(message);
-      }
+    });
+
+    // If there is no message selected, disable the next button
+    if (!message) {
+      connection.trigger("updateButton", { button: "done", enabled: false });
+      // If there is a message, skip to the summary step
+    } else {
+      $("#select1")
+        .find("option[value=" + message + "]")
+        .attr("selected", "selected");
+      $("#message").html(message);
     }
-  
-    function onGetTokens(tokens) {
-      // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-      // console.log(tokens);
-    }
-  
-    function onGetEndpoints(endpoints) {
-      // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-      // console.log(endpoints);
-    }
-  
-    function onClickedNext() {
-        save();
-    }
-  
-    function save() {
-      var name = $("#select1").find("option:selected").html();
-      var value = getMessage();
-  
-      // 'payload' is initialized on 'initActivity' above.
-      // Journey Builder sends an initial payload with defaults
-      // set by this activity's config.json file.  Any property
-      // may be overridden as desired.
-      payload.name = name;
-  
-      payload["arguments"].execute.inArguments = [{ message: value }];
-  
-      payload["metaData"].isConfigured = true;
-  
-      connection.trigger("updateActivity", payload);
-    }
-  
-    function getMessage() {
-      return $("#select1").find("option:selected").attr("value").trim();
-    }
-  });
+  }
+
+  function onGetTokens(tokens) {
+    // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
+    // console.log(tokens);
+  }
+
+  function onGetEndpoints(endpoints) {
+    // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
+    // console.log(endpoints);
+  }
+
+  function onClickedNext() {
+    save();
+  }
+
+  function save() {
+    var name = $("#select1").find("option:selected").html();
+    var value = getMessage();
+
+    // 'payload' is initialized on 'initActivity' above.
+    // Journey Builder sends an initial payload with defaults
+    // set by this activity's config.json file.  Any property
+    // may be overridden as desired.
+    payload.name = name;
+
+    payload["arguments"].execute.inArguments = [{ message: value }];
+
+    payload["metaData"].isConfigured = true;
+
+    connection.trigger("updateActivity", payload);
+  }
+
+  function getMessage() {
+    return $("#select1").find("option:selected").attr("value").trim();
+  }
+});
